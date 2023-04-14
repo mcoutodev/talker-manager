18c842b411c7d9138337e4fb871da17e32455593
Marlon Couto <marloncouto199@gmail.com>
2023-04-14 16:57:46 -0300

    feat: adiciona função para ler arquivos JSON

---
+++ src/utils/fs/readJson.js
@@ -0,0 +1,12 @@
+const fs = require('fs').promises;
+
+const readJson = async (path) => {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+module.exports = readJson;
