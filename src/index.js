72df78b215596ef7970f35e34843496f9c0e3c77
Marlon Couto <marloncouto199@gmail.com>
2023-04-14 16:56:43 -0300

    feat: adiciona rota para resgatar palestrantes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const {
+  HTTP_OK_STATUS,
+  HTTP_ERROR_STATUS,
+  PORT,
+} = require('./utils/variables');
+const readJson = require('./utils/fs/readJson');
 
 const app = express();
 app.use(express.json());
 
-const HTTP_OK_STATUS = 200;
-const PORT = process.env.PORT || '3001';
+const talkerJson = './src/talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const data = await readJson(talkerJson);
+    return res.status(HTTP_OK_STATUS).send(data || []);
+  } catch (err) {
+    return res.status(HTTP_ERROR_STATUS);
+  }
 });
 
 app.listen(PORT, () => {
