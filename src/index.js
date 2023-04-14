820320c7008e066079464f8f9058b715aa0b8dee
Marlon Couto <marloncouto199@gmail.com>
2023-04-14 17:54:31 -0300

    feat: adiciona rota para resgatar um palestrante

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   HTTP_OK_STATUS,
   HTTP_ERROR_STATUS,
+  HTTP_NOT_FOUND,
   PORT,
 } = require('./utils/variables');
 const readJson = require('./utils/fs/readJson');
@@ -11,6 +12,20 @@ app.use(express.json());
 
 const talkerJson = './src/talker.json';
 
+const findTalker = async (req, res, next) => {
+  const { id } = req.params;
+  const talkers = await readJson(talkerJson);
+  const talkerFound = talkers.find((talker) => talker.id === Number(id));
+  
+  if (!talkerFound) {
+    return res.status(HTTP_NOT_FOUND).send({ 
+      message: 'Pessoa palestrante não encontrada', 
+    });
+  }
+  res.locals = talkerFound;
+  next();
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -18,8 +33,16 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (_req, res) => {
   try {
-    const data = await readJson(talkerJson);
-    return res.status(HTTP_OK_STATUS).send(data || []);
+    const talkers = await readJson(talkerJson);
+    return res.status(HTTP_OK_STATUS).send(talkers || []);
+  } catch (err) {
+    return res.status(HTTP_ERROR_STATUS);
+  }
+});
+
+app.get('/talker/:id', findTalker, (_req, res) => {
+  try {
+    return res.status(HTTP_OK_STATUS).send(res.locals);
   } catch (err) {
     return res.status(HTTP_ERROR_STATUS);
   }
