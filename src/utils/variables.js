247d8001a2f7057fcad18348feab6857f3319d01
Marlon Couto <marloncouto199@gmail.com>
2023-04-14 17:50:55 -0300

    feat: adiciona variável para status code 404

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,9 +1,11 @@
 const HTTP_OK_STATUS = 200;
 const HTTP_ERROR_STATUS = 500;
+const HTTP_NOT_FOUND = 404;
 const PORT = process.env.PORT || '3001';
 
 module.exports = {
   HTTP_OK_STATUS,
   HTTP_ERROR_STATUS,
+  HTTP_NOT_FOUND,
   PORT,
 };
